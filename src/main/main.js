f66274d1ba96da45ce9a0b7829c101d92366ea9f
Morgan A Ray <andreeray@live.com>
2025-02-10 19:37:41 +0100

    Update at 2025-02-10T18:37:41.356Z

--- src/main/main.js
+++ src/main/main.js
@@ -171,7 +171,7 @@ ipcMain.handle("get-settings", () => {
 });
 
 ipcMain.on("update-settings", (event, newSettings) => {
-    // log.log("Updating Settings:", newSettings )
+    log.log("Updating Settings:", newSettings )
     appSettings = { ...appSettings, ...newSettings };
     saveSettings();
 });
