372357d5bdab0e95ee1d099d16aefcee233003ec
Morgan A Ray <andreeray@live.com>
2025-02-12 21:12:57 +0100

    Update at 2025-02-12T20:12:57.718Z

--- src/main/main.js
+++ src/main/main.js
@@ -327,7 +327,7 @@ app.on("ready", () => {
         log.log("Splash screen closed. Loading main app...");
 
         log.log("Starting ticker collection...");
-        // collectTickers(); // ✅ Start ticker collection
+        collectTickers(); // ✅ Start ticker collection
 
         windows.docker = createWindow("docker", () => createDockerWindow(isDevelopment));
         windows.settings = createWindow("settings", () => createSettingsWindow(isDevelopment));
