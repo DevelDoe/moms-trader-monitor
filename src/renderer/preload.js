37186a1f0c6038b39f98535fcd5a3d5f652bd82d
Morgan A Ray <andreeray@live.com>
2025-02-12 19:04:32 +0100

    Update at 2025-02-12T18:04:31.998Z

--- src/renderer/preload.js
+++ src/renderer/preload.js
@@ -8,7 +8,7 @@ contextBridge.exposeInMainWorld("electronAPI", {
 contextBridge.exposeInMainWorld("topAPI", {
     toggle: () => ipcRenderer.send("toggle-top"),
     refresh: () => ipcRenderer.send("refresh-top"),
-    getTickers: (listType) => ipcRenderer.invoke("get-tickers", listType), 
+    getTickers: (listType) => ipcRenderer.invoke("get-tickers", listType),
     onTickerUpdate: (callback) => ipcRenderer.on("tickers-updated", callback),
     clearSession: () => ipcRenderer.send("clear-session"),
     onSessionCleared: (callback) => ipcRenderer.on("session-cleared", callback),
@@ -16,19 +16,21 @@ contextBridge.exposeInMainWorld("topAPI", {
     onFilterUpdate: (callback) => ipcRenderer.on("filter-updated", callback),
 });
 
+contextBridge.exposeInMainWorld("newsAPI", {
+    get: () => ipcRenderer.invoke("get-all-news"),
+});
+
 contextBridge.exposeInMainWorld("settingsAPI", {
     toggle: () => ipcRenderer.send("toggle-settings"),
     get: () => ipcRenderer.invoke("get-settings"),
     update: (settings) => ipcRenderer.send("update-settings", settings),
     getAttributes: (listType) => ipcRenderer.invoke("get-attributes", listType),
-    onAttributesUpdate: (callback) => ipcRenderer.on("tickers-updated", () => {
-        console.log("🔄 Attributes updated, refreshing settings...");
-        callback();
-    }),
+    onAttributesUpdate: (callback) =>
+        ipcRenderer.on("tickers-updated", () => {
+            console.log("🔄 Attributes updated, refreshing settings...");
+            callback();
+        }),
     onUpdate: (callback) => ipcRenderer.on("settings-updated", (_, updatedSettings) => callback(updatedSettings)),
 });
 
-contextBridge.exposeInMainWorld("newsAPI", {
-    get: (listType) => ipcRenderer.invoke("get-all-news"),
-});
 
