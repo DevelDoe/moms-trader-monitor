f3f7b61bde861b1999899c2d6bfe0ff9b85c2307
Morgan A Ray <andreeray@live.com>
2025-02-12 21:14:46 +0100

    Update at 2025-02-12T20:14:46.392Z

--- src/main/collectors/news.js
+++ src/main/collectors/news.js
@@ -42,7 +42,7 @@ const connectAlpacaNews = () => {
             const filteredNews = data.filter((news) => news.T === "n" && news.symbols.some((symbol) => trackedTickers.has(symbol)));
 
             if (filteredNews.length > 0) {
-                log.log(`📨 Received ${filteredNews.length} relevant news updates.`);
+                log.log(`📨 Received ${filteredNews.length} relevant news updates`, filteredNews);
                 filteredNews.forEach(handleNewsData);
             }
         }
