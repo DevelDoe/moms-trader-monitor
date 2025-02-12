b27b3fd641e3febed31ea12087a3e9104cbba09d
Morgan A Ray <andreeray@live.com>
2025-02-12 22:43:29 +0100

    Update at 2025-02-12T21:43:29.257Z

--- src/main/main.js
+++ src/main/main.js
@@ -258,7 +258,7 @@ ipcMain.handle("get-all-news", () => {
 // });
 
 tickerStore.on("newsUpdated", ({ ticker, newsItems }) => {
-    log.log("Broadcasting news update:", ticker, newsItems); // Debug Log
+    log.log("Broadcasting news update"); // Debug Log
     BrowserWindow.getAllWindows().forEach((win) => {
         win.webContents.send("news-updated", { ticker, newsItems });
     });
