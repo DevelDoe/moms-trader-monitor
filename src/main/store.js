cca6453efdcf67c83d31f7d1631346781f0dc96c
Morgan A Ray <andreeray@live.com>
2025-02-12 22:24:05 +0100

    Update at 2025-02-12T21:24:05.002Z

--- src/main/store.js
+++ src/main/store.js
@@ -113,7 +113,7 @@ class Store extends EventEmitter {
     }
 
     cleanupOldNews() {
-        const TWENTY_MINUTES = 20 * 60 * 1000;
+        const TWENTY_MINUTES = 1 * 60 * 1000;
         const now = Date.now();
 
         this.newsData.forEach((newsArray, ticker) => {
