5643cfd460373418d749f0cab2430350ecfb830a
Morgan A Ray <andreeray@live.com>
2025-02-13 22:09:45 +0100

    Update at 2025-02-13T21:09:45.192Z

--- src/renderer/preload.js
+++ src/renderer/preload.js
@@ -13,7 +13,6 @@ contextBridge.exposeInMainWorld("topAPI", {
     clearSession: () => ipcRenderer.send("clear-session"),
     onSessionCleared: (callback) => ipcRenderer.on("session-cleared", callback),
     applyFilters: (min, max) => ipcRenderer.send("apply-filters", { min, max }),
-    onFilterUpdate: (callback) => ipcRenderer.on("filter-updated", callback),
 });
 
 contextBridge.exposeInMainWorld("newsAPI", {
