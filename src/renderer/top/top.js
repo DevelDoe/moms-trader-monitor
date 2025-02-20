8e9d165a0f394b5274dfd4beb0bdcb207b4c7f6a
Morgan A Ray <andreeray@live.com>
2025-02-20 23:13:27 +0100

    Update at 2025-02-20T22:13:27.040Z

--- src/renderer/top/top.js
+++ src/renderer/top/top.js
@@ -188,7 +188,7 @@ function updateTickersTable(tickers, tableId, prevTickers) {
 
     const allColumns =
         tableId === "tickers-all"
-            ? [...new Set(tickers.flatMap((t) => Object.keys(t)))].filter((key) => key !== "Bonuses" && key !== "Time") // ✅ Exclude Bonuses
+            ? [...new Set(tickers.flatMap((t) => Object.keys(t)))].filter((key) => key !== "Bonuses" && key !== "Time" && key !== "HasNews") // ✅ Exclude Bonuses
             : [...new Set([...Object.keys(tickers[0]), "Bonuses"])].filter((key) => enabledColumns[key] || key === "Symbol");
 
     // ✅ Generate the header dynamically
