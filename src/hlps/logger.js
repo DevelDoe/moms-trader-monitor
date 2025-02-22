b1955c517b7460c3ce42e06c6b275b67aed04b61
Morgan A Ray <andreeray@live.com>
2025-02-22 13:29:26 +0100

    Update at 2025-02-22T12:29:26.699Z

--- src/hlps/logger.js
+++ src/hlps/logger.js
@@ -22,8 +22,9 @@ if (!fs.existsSync(logDir)) {
 
 // ✅ List of scripts to suppress logging from
 // const SUPPRESS_LOGGING_FROM = new Set(["main.js", "tickers.js", "store.js", "news.js", "alpha.js"]); 
-// const SUPPRESS_LOGGING_FROM = new Set(["main.js", "tickers.js", "store.js", "news.js"]); 
-const SUPPRESS_LOGGING_FROM = new Set(["main.js", "tickers.js",  "news.js"]); 
+// const SUPPRESS_LOGGING_FROM = new Set(["main.js", "tickers.js", "store.js", "news.js"]); // alpha only 
+// const SUPPRESS_LOGGING_FROM = new Set(["main.js", "tickers.js",  "news.js"]); 
+// const SUPPRESS_LOGGING_FROM = new Set(["main.js", "tickers.js"]); 
 
 /**
  * Writes log messages to a file in production mode.
