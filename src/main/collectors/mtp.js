3cac9ffaf6faa01e2844fae7402b6ba0a25160a1
Morgan A Ray <andreeray@live.com>
2025-02-23 01:03:09 +0100

    Update at 2025-02-23T00:03:09.179Z

--- src/main/collectors/mtp.js
+++ src/main/collectors/mtp.js
@@ -4,15 +4,21 @@ const createLogger = require("../../hlps/logger");
 const log = createLogger(__filename);
 
 const connectMTP = () => {
+    // Create a WebSocket connection to the server
+    const ws = new WebSocket("ws://localhost:9090"); // Replace with your actual WebSocket URL
 
-}
+    // Handle WebSocket connection open
+    ws.on("open", () => {
+        log.log("Connected to WebSocket server.");
+    });
+
+    
+};
 
 function getMtpOverview(ticker) {
     const store = require("../store");
 
-
     store.addMtpOverviwe(newsArray);
-
 }
 
 module.exports = { connectMTP };
