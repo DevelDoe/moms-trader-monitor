86ab7d9048b0444ae795d987f85a7dde681863ac
Morgan A Ray <andreeray@live.com>
2025-02-23 01:11:16 +0100

    Update at 2025-02-23T00:11:16.719Z

--- src/hlps/logger.js
+++ src/hlps/logger.js
@@ -21,7 +21,7 @@ if (!fs.existsSync(logDir)) {
 }
 
 // ✅ List of scripts to allow logging from
-const ALLOWED_LOGGING_FROM = new Set(["main.js", "tickers.js", "store.js", "news.js", "alpha.js"]); // Add scripts here
+const ALLOWED_LOGGING_FROM = new Set(["news.js"]); // Add scripts here
 
 /**
  * Writes log messages to a file in production mode.
