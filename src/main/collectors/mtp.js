e411412a73934faa00032731823bc87c6c6c6ca6
Morgan A Ray <andreeray@live.com>
2025-04-08 09:32:38 +0200

    mornign

--- src/main/collectors/mtp.js
+++ src/main/collectors/mtp.js
@@ -32,7 +32,7 @@ function debounce(func, delay) {
 
 // mtp.js - Fixed version
 const connectMTP = (scannerWindow, focusWindow) => {
-    const clientId = "MTM-DEV";
+    const clientId = "MTM-PROD";
     let ws;
 
     // Helper function for safe JSON parsing
