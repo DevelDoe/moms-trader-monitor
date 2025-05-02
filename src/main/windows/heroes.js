05cd5e7946fea85b4a523aba72000faaa58b01f1
Morgan A Ray <andreeray@live.com>
2025-05-03 01:12:56 +0200

    2025-05-02-23-12-56

---
+++ src/main/windows/heroes.js
@@ -0,0 +1,52 @@
+// ./src/main/windows/heroes.js
+
+const { BrowserWindow } = require("electron");
+const path = require("path");
+const { getWindowState, setWindowBounds } = require("../utils/windowState");
+
+function createHeroesWindow(isDevelopment) {
+    const state = getWindowState("heroesWindow");
+
+    const window = new BrowserWindow({
+        width: state.width || 850,
+        height: state.height || 660,
+        x: state.x,
+        y: state.y,
+        frame: false,
+        alwaysOnTop: false,
+        resizable: true,
+        transparent: false,
+        hasShadow: false,
+        roundedCorners: false,
+        backgroundColor: "#00000000",
+        useContentSize: true,
+        webPreferences: {
+            preload: path.join(__dirname, "../../renderer/preload.js"),
+            contextIsolation: true,
+            enableRemoteModule: false,
+            nodeIntegration: false,
+        },
+    });
+
+    window.loadFile(path.join(__dirname, "../../renderer/heroes/heroes.html"));
+
+    if (isDevelopment) {
+        window.webContents.once("did-finish-load", () => {
+            window.webContents.openDevTools({ mode: "detach" });
+        });
+    }
+
+    window.on("move", () => {
+        const bounds = window.getBounds();
+        setWindowBounds("heroesWindow", bounds);
+    });
+
+    window.on("resize", () => {
+        const bounds = window.getBounds();
+        setWindowBounds("heroesWindow", bounds);
+    });
+
+    return window;
+}
+
+module.exports = { createHeroesWindow };
