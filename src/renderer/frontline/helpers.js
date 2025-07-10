7840212900170d124ec19f69731fdae76431768d
Morgan A Ray <andreeray@live.com>
2025-07-11 01:48:34 +0200

    vol

--- src/renderer/frontline/helpers.js
+++ src/renderer/frontline/helpers.js
@@ -132,9 +132,9 @@
     }
     
     function abbreviatedValues(num) {
-        if (num >= 1e6) return (num / 1e6).toFixed(1) + "M";
-        if (num >= 1e3) return (num / 1e3).toFixed(1) + "K";
-        return num.toString();
+        if (num < 1000) return num.toString();          // No abbreviation under 1K
+        if (num < 1_000_000) return (num / 1_000).toFixed(1) + "K";
+        return (num / 1_000_000).toFixed(1) + "M";
     }
 
     function getSymbolColor(hue) {
